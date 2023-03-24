3e655531f76dc97a333ed921e586485be8c3a09a
MauLight <maulisseluz@gmail.com>
2023-03-24 11:48:49 -0300

    components created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [darkTheme, setDarkTheme] = useState(true);
+
+  function toggleTheme() {
+    setDarkTheme(prevDarkTheme => !prevDarkTheme);
+  };
+
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <ThemeContext.Provider value={darkTheme}>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <FunctionContextComponent />
+      <ClassContextComponent />
+    </ThemeContext.Provider>
+    </>
   );
 }
 

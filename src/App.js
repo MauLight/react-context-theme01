ecd768247452803850746e2cb209cfc0870d180b
MauLight <maulisseluz@gmail.com>
2023-03-24 13:36:39 -0300

    first context exercise done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-//import { useState } from 'react';
+import { useContext, useState } from 'react';
 import './App.css';
-import Heading from './components/Heading';
-import { Section } from './components/Section';
+//import Heading from './components/Heading';
+import { places } from './components/places';
+import { SizeContext } from './components/SizeContext';
+//import { Section } from './components/Section';
+import { getImageUrl } from './utils';
 
-export default function ProfilePage() {
+/*
+
+function ProfilePage() {
 
   return (
     <Section>
@@ -38,4 +43,57 @@ const Post = ({ title, body }) => {
   );
 }
 
+*/
 
+const App = () => {
+
+  const [isLarge, setIsLarge] = useState(false);
+  const imageSize = isLarge ? 150 : 100;
+
+  return (
+    <>
+      <SizeContext.Provider value={imageSize}>
+        <label>
+          <input
+            type="checkbox"
+            checked={isLarge}
+            onChange={e => { setIsLarge(e.target.checked) }}
+          />
+          Use Large Images
+        </label>
+        <hr />
+        <List imageSize={imageSize} />
+      </SizeContext.Provider>
+    </>
+  )
+}
+
+const List = ({ imageSize }) => {
+
+  const listItems = places.map(place => <li key={place.id} ><Place place={place} imageSize={imageSize} /></li>);
+
+  return <ul>{listItems}</ul>;
+};
+
+const Place = ({ place, imageSize }) => {
+  return (
+    <>
+      <PlaceImage place={place} imageSize={imageSize} />
+      <p>
+        <b>{place.name}</b>
+        {': ' + place.description}
+      </p>
+    </>
+  );
+}
+
+const PlaceImage = ({ place, imageSize }) => {
+
+  const size = useContext(SizeContext);
+
+  return (
+    <img src={getImageUrl(place)} alt={place.name} width={size} height={size} />
+  );
+}
+
+export default App;
---
+++ src/utils.js
@@ -0,0 +1,5 @@
+export const getImageUrl = (place) => {
+    return (
+        'https://i.imgur.com/' + place.imageId + 'l.jpg'
+    );
+}
